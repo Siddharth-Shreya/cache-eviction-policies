faf9fb1470842fcb7b36d834b89010c053d0d7af
SidSun13 <siddharthsundar2@gmail.com>
2023-12-17 22:48:52 -0800

    Added LinkedList class

---
+++ LinkedList.java
@@ -0,0 +1,10 @@
+/*
+This code creates a linked list in Java.
+This will be used to simulate the cache.
+*/
+
+public class LinkedList{
+    public static void main(String[] args){
+        System.out.println("Shreya is kinda weird");
+    }
+}
