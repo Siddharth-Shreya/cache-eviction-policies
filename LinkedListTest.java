85add766404c4defe8c543bc5b6395cbafdf92d8
SidSun13 <siddharthsundar2@gmail.com>
2023-12-26 15:28:39 -0800

    Updated LinkedList.java

---
+++ LinkedListTest.java
@@ -0,0 +1,11 @@
+public class LinkedListTest{
+    public static void main (String[] args){
+        LinkedList A = new LinkedList();
+        LinkedList B = new LinkedList();
+        LinkedList C = null;
+        for(int i = 1; i <= 10; i++){
+            A.append(Integer.toString(i));
+            
+        }
+    }
+}
