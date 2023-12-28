7411b9e88f8c13b0d5d3465c0f100c3f91732607
shreyasun <shreyasundar27@gmail.com>
2023-12-28 14:06:23 -0800

    Tested

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -23,7 +23,7 @@ public class LinkedListTest{
             9
             10
         */
-
+        System.out.println("-----------");
         A.moveFront();
         System.out.println("Cursor Index after moving front: " + A.position());
         /* 0 */
@@ -39,41 +39,46 @@ public class LinkedListTest{
             Value at cursor after moving 3 spaces: 4
             Value at cursor after moving 4 spaces: 5
         */
-        System.out.println("Current cursor element: " + A.getValueAtCursor() + ", Position: " + A.position()); /* 6 */
+        System.out.println("Current cursor element: " + A.getValueAtCursor() + ", Position: " + A.position()); 
+        /* Current cursor element: 6, Position: 5 */
+        System.out.println("-----------");
         System.out.println("Inserting -1 before cursor element...");
         A.insertBefore("-1");
         System.out.println("List A after inserting -1: " + A); /* 1 2 3 4 5 -1 6 7 8 9 10 */
         System.out.println("Position: " + A.position());
         System.out.println("Length of List A: " + A.length()); /* 11 */
-
-
         System.out.println("Current cursor element: " + A.getValueAtCursor()); /* 6 */
-        System.out.println("Current cursor position: " + A.position());
+        System.out.println("Current cursor position: " + A.position()); /* 6 */
 
-        System.out.println("Deleting front of list A...");
-        
-        A.deleteFront();
         System.out.println("-----------");
+        System.out.println("Deleting front of list A...");
+        A.deleteFront();
         System.out.println("List A after deleting front element: " + A); /* 2 3 4 5 -1 6 7 8 9 10 */
-        System.out.println("Cursor Index after deleting front: " + A.position()); /*  */
+        System.out.println("Cursor Index after deleting front: " + A.position()); /* 5 */
+        System.out.println("Front of List A now: " + A.front()); /* 2 */
+        System.out.println("Length of List A after deleting an element: " + A.length()); /* 10 */
 
-        System.out.println("Front of List A now: " + A.front());
-        System.out.println("Length of List A after deleting an element: " + A.length());
-
+        System.out.println("-----------");
+        System.out.println("Inserting -2 after cursor element...");
         A.insertAfter("-2");
-        System.out.println("Value at cursor new: " + A.getValueAtCursor());
+        System.out.println("List A now: " + A);
+        System.out.println("Value at cursor now: " + A.getValueAtCursor()); /* 6 */
+        System.out.println("Value at index 7 now: " + A.getValueAtIndex(7));      
 
-        for(int i = 0; i < 3; i++){
+
+        System.out.println("-----------");
+        for(int i = 0; i < 4; i++){
             A.movePrev();
         }
-
-        System.out.println("Value at cursor after moving prev 3 spaces: " + A.getValueAtCursor());
+        System.out.println("Value at cursor after moving prev 4 spaces: " + A.getValueAtCursor());
+        
+        System.out.println("-----------");
         System.out.println("List A now: " + A);
-
         System.out.println("Set index 5 to -3:");
         A.setValueAtIndex("-3", 5);
         System.out.println("List A now: " + A);
 
+        System.out.println("-----------");
         System.out.println("Deleting 3 elements from back of List A...");
         for(int i = 0; i < 3; i++) {
             A.deleteBack();
@@ -81,11 +86,11 @@ public class LinkedListTest{
         System.out.println("List A now: " + A);
         System.out.println("Back of List A now: " + A.back());
 
+        System.out.println("-----------");
         System.out.println("Now we will delete value at cursor index: " + A.getValueAtCursor());
-
         A.delete();
-
         System.out.println("List A after deleting cursor element " + A);
-
+        
+        System.out.println("-----------");
     }
 }
